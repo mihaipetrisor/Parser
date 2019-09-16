ef26691faa3529487ed4ef9fc93b5cf91a0d3e0c
Admin <Admin@DEVWIN7U.localdomain>
2019-09-16 14:40:10 +0300

    added and committed

--- src/Parser.java
+++ src/Parser.java
@@ -8,6 +8,9 @@ import reformatstring.JavaParser;
 import reformatstring.StreamReader;
 import reformatstring.StreamWriter;
 
+
+// TEST COMMIT
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
